23b8e0bfde6667e1ae0caa1bfc3d3aae5942883c
Shawn Yu <shawnyudesign@gmail.com>
2020-04-05 13:15:39 -0400

    sdsd

--- server.js
+++ server.js
@@ -11,50 +11,16 @@ useNewUrlParser: true,useUnifiedTopology: true,useUnifiedTopology: true},);
 // mongoose.set("useFindAndModify",false);
 
 var activityList = {"strength":["push ups","pull ups","dead lift","bench lift"],"endurance":["jogging","cycling","jump roping"],"balance":["foottaps","sit to stand","standing march"],"flexibility":["yoga","stretching","tai-chi"]};
-db.ActivityList.findOneAndUpdate({name:"ListToChooseFrom"},{list:JSON.stringify(activityList),name:"ListToChooseFrom"},{new: true, upsert:true})
+db.ActivityList.findOneAndUpdate({name:"ListToChooseFrom"},{list:JSON.stringify(activityList),name:"ListToChooseFrom"},{new: true, upsert:true},
+db.AllLog.findOneAndUpdate({name:"AllOfLogs"},{new: true, upsert:true})
+)
   .then(dbactivitylists => {
     console.log(dbactivitylists );
   })
   .catch(({message}) => {
     console.log(message);
   });
-  db.Strength.create({ name: "Strength" })
-  .then(dbStrength => {
-    console.log(dbStrength);
-  })
-  .catch(({message}) => {
-    console.log(message);
-  });
-  db.Endurance.create({ name: "Endurance" })
-  .then(dbEndurance => {
-    console.log(dbEndurance);
-  })
-  .catch(({message}) => {
-    console.log(message);
-  });
-  db.Balance.create({ name: "Balance" })
-  .then(dbBalance => {
-    console.log(dbBalance);
-  })
-  .catch(({message}) => {
-    console.log(message);
-  });
-
-  db.Flexibility.create({ name: "Flexibility" })
-  .then(dbFlexibility => {
-    console.log(dbFlexibility);
-  })
-  .catch(({message}) => {
-    console.log(message);
-  });
-
-  db.AllLog.findOneAndUpdate({name:"AllOfLogs"},{new: true, upsert:true})
-  .then(dbAllLog=>{
-    console.log(dbAllLog);
-  })
-  .catch(({message}) => {
-    console.log(message);
-});
+ 
   
 
 const app = express();
